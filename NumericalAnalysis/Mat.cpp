eaf0b36775a9f855baf3c5c6aec9ac19aa4ddece
southgnss_mozh <mozh@southgnss.com>
2018-07-31 16:07:34 +0800

    Mat初始操作

--- NumericalAnalysis/Mat.cpp
+++ NumericalAnalysis/Mat.cpp
@@ -2,6 +2,11 @@
 
 
 
+int Mat::getIndex(int i, int j)
+{
+	return i *width + j;
+}
+
 Mat::Mat()
 {
 }
@@ -10,3 +15,64 @@ Mat::Mat()
 Mat::~Mat()
 {
 }
+
+Mat::Mat(int w, int h)
+{
+	 width=w;
+	 height = h;
+	 itemsum = w*h;
+	 data = new double[itemsum];
+}
+
+Mat::Mat(const Mat & a)
+{
+	/*for (int i = 0; i < _width; i++) {
+		for (int j = 0; j < _height; j++) {
+			data[getIndex(i, j)] = a.data[getIndex(i, j)];
+		}
+	}*/
+	data = new double[itemsum];
+	for (int i = 0; i < itemsum; i++) {
+		data[i] = a.data[i];
+	}
+}
+
+void Mat::zeros(void)
+{
+	for (int i = 0; i < itemsum; i++)
+		data[i] = 0.0;
+}
+
+Mat Mat::operator+(const Mat& a)
+{
+	Mat mat;
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			mat.data[getIndex(i, j)] = data[getIndex(i, j)] + a.data[getIndex(i, j)];
+		}
+	}
+	return mat;
+}
+Mat Mat::operator-(const Mat& a)
+{
+	Mat mat;
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			mat.data[getIndex(i, j)] = data[getIndex(i, j)] - a.data[getIndex(i, j)];
+		}
+	}
+	return mat;
+}
+Mat Mat::operator*(const Mat& a)
+{
+	Mat mat = Mat(width, a.height);
+	mat.zeros();
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+		    for(int k=0;k<a.height;k++)
+			mat.data[getIndex(i, k)] = data[getIndex(i, j)] - a.data[getIndex(j, k)];
+		}
+	}
+	return mat;
+}
+
--- NumericalAnalysis/Mat.h
+++ NumericalAnalysis/Mat.h
@@ -1,8 +1,19 @@
 #pragma once
 class Mat
 {
+private:
+	
+	int getIndex(int i, int j);
 public:
 	Mat();
 	~Mat();
+	Mat(int width, int height);
+	Mat(const Mat& a);
+	void zeros(void);
+	Mat operator+(const Mat &a);
+	Mat operator-(const Mat &b);
+	Mat operator*(const Mat &b);
+	double *data;
+	int width; int height; int itemsum;
 };
 
